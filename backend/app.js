8fd348d06c45f0a3a2d99006c8226735408152fd
Qassam Deeb <Qassamdeeb@gmailcom>
2025-05-25 17:32:34 +0300

    update image 7

--- backend/app.js
+++ backend/app.js
@@ -37,7 +37,7 @@ app.options("*", cors());
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
 app.use(authJwt());
-app.use("/public/uploads", express.static((__dirname, "public/uploads")));
+app.use("/public", express.static((__dirname, "public")));
 
 app.use((req, res, next) => {
   res.setHeader(
