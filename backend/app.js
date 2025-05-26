2bc79c3f6132e7c9cd695f06f34af9026ca54d3b
Qassam Deeb <Qassamdeeb@gmailcom>
2025-05-26 19:52:18 +0300

    update image 8

--- backend/app.js
+++ backend/app.js
@@ -22,12 +22,25 @@ app.enable("trust proxy");
 // Update CORS configuration
 app.use(
   cors({
-    origin: [
-      "http://localhost:4173",
-      "https://your-production-frontend-url.com",
-      "http://localhost:4174",
-      "http://localhost:5173",
-    ],
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        "http://localhost:4173",
+        "http://localhost:4174",
+        "http://localhost:5173",
+        "https://e-shop-lbbw.onrender.com",
+      ];
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        return callback(
+          new Error(
+            "The CORS policy for this site does not allow access from the specified Origin."
+          ),
+          false
+        );
+      }
+      return callback(null, true);
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: [
       "Content-Type",
